8e938502035aab521256ecd36d9652b9e488e0f1
natacoop <nat@natcooper.com>
2015-12-20 16:30:34 -0500

    site revamp 2016...

---
+++ js/scripts.js
@@ -0,0 +1,116 @@
+// ANIMATED GRADIENT BACKGROUND - original code by Mario Klingemann (@quasimondo): https://twitter.com/quasimondo | http://codepen.io/quasimondo/pen/lDdrF
+
+// ORIGINAL COLOURS
+// var colors = new Array(
+//   [62,35,255],
+//   [60,255,60],
+//   [255,35,98],
+//   [45,175,230],
+//   [255,0,255],
+//   [255,128,0]);
+
+var colors = new Array(
+  [117,123,171],
+  [72,108,123],
+  [146,120,158],
+  [92,137,154],
+  [96,90,138],
+  [99,155,142]);
+
+var step = 0;
+//color table indices for: 
+// current color left
+// next color left
+// current color right
+// next color right
+var colorIndices = [0,1,2,3];
+
+//transition speed
+var gradientSpeed = 0.0009;
+
+function updateGradient()
+{
+  
+  if ( $===undefined ) return;
+  
+var c0_0 = colors[colorIndices[0]];
+var c0_1 = colors[colorIndices[1]];
+var c1_0 = colors[colorIndices[2]];
+var c1_1 = colors[colorIndices[3]];
+
+var istep = 1 - step;
+var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+var color2 = "rgb("+r2+","+g2+","+b2+")";
+
+ $('#gradient').css({
+   background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+    background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+  
+  step += gradientSpeed;
+  if ( step >= 1 )
+  {
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[2] = colorIndices[3];
+    
+    //pick two new target color indices
+    //do not pick the same as the current one
+    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    
+  }
+}
+
+setInterval(updateGradient,10);
+
+
+
+// SMOOTH SCROLL
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+// EXPAND-COLLAPSE
+
+$('.folio-wrap').click(function(){
+    $('ul.expanded').slideUp(1000);
+    if ($(this).siblings('ul.expanded').is(":hidden")) {
+      $(this).siblings('ul.expanded').slideToggle(1000);
+    }
+    $(this).siblings('.folio-wrap i').toggleClass('fa-angle-down');
+    $(this).siblings('.folio-wrap i').toggleClass('fa-angle-up');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
